0d99aba314ca75f3eb09c6401437e669a6511990
Aleksandr <shohin-sasha1993@yandex.ru>
2022-03-13 23:33:51 +0300

    Add Repository

---
+++ MarvelousService.DataLayer/Repositories/Interfaces/IServiceRepository.cs
@@ -0,0 +1,18 @@
+﻿using MarvelousService.DataLayer.Entities;
+using MarvelousService.DataLayer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarvelousService.DataLayer.Interfaces
+{
+    public interface IServiceRepository
+    {
+        public Service GetServiceById(int id);
+        int AddService(Service service);        
+        List<Service> GetByLeadId(int id);
+        
+    }
+}
