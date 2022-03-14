3cfc237fdda8b19295ff035f2e19f1993f5d6e2a
Aleksandr <shohin-sasha1993@yandex.ru>
2022-03-14 18:33:25 +0300

    Add Exception in BLL and API

---
+++ MarvelousService.Business/Exceptions/BusinessException.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MarvelousService.BusinessLayer.Exceptions
+{
+    public class BusinessException : Exception
+    {
+        public BusinessException(string message) : base(message)
+        {
+
+        }
+    }
+}
--- MarvelousService/Program.cs
+++ MarvelousService/Program.cs
@@ -1,6 +1,9 @@
 using MarvelousService.API.Infrastructure;
 using MarvelousService.API.Extensions;
 using MarvelousService.DataLayer.Configuration;
+using System.Text.Json.Serialization;
+using MarvelousService.API.ExceptionResponse;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +19,23 @@ builder.Services.AddSwaggerGen(config =>
 {
     config.EnableAnnotations();
 });
+
+builder.Services.AddMvc()
+    .AddJsonOptions(options =>
+    {
+
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+
+    })
+
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.InvalidModelStateResponseFactory = context =>
+        {
+            var exc = new ValidationExceptionResponse(context.ModelState);
+            return new UnprocessableEntityObjectResult(exc);
+        };
+    });
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
