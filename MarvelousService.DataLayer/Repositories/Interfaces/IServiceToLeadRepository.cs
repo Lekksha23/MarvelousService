eee47f692435db48c7c0704999f1b8dd0ef70f90
Aleksandr <shohin-sasha1993@yandex.ru>
2022-03-15 22:59:22 +0300

    Add Table Period ,Entity and metod in ServiceToLeadRepo

--- MarvelousService.DataLayer/Repositories/Interfaces/IServiceToLeadRepository.cs
+++ MarvelousService.DataLayer/Repositories/Interfaces/IServiceToLeadRepository.cs
@@ -11,7 +11,8 @@ namespace MarvelousService.DataLayer.Interfaces
     public interface IServiceToLeadRepository
     {
         public ServiceToLead GetServiceToLeadById(int id);
-        int AddServiceToLead(ServiceToLead service);        
+        int AddServiceToLead(ServiceToLead service);
+        int AddPeriod(Period period);
         List<ServiceToLead> GetByLeadId(int id);
         
     }
