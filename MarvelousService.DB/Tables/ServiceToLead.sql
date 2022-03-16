be4c4eb43de6ec353e5f84d1924d9c3bcea79deb
Lekksha <bennykorovin@gmail.com>
2022-03-16 12:59:20 +0300

    tables are ready

--- MarvelousService.DB/Tables/ServiceToLead.sql
+++ MarvelousService.DB/Tables/ServiceToLead.sql
@@ -1,11 +1,12 @@
 ﻿CREATE TABLE [dbo].[ServiceToLead]
 (
-	[Id] INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	[Id] int NOT NULL IDENTITY(1, 1) PRIMARY KEY,
 	[Period] int NOT NULL,
 	[Price] decimal(10,0) NOT NULL,
 	[Status] int NOT NULL,
 	[ServiceId] int NOT NULL,
 	[LeadId] int NOT NULL,
 	[TransactionId] int NOT NULL
-	CONSTRAINT [FK_ServiceToLead_Service] FOREIGN KEY ([ServiceId]) REFERENCES [dbo].[Service]([Id])
+	CONSTRAINT [FK_ServiceToLead_Service] FOREIGN KEY ([ServiceId]) REFERENCES [dbo].[Service]([Id]),
+	CONSTRAINT [FK_ServiceToLead_Transaction] FOREIGN KEY ([TransactionId]) REFERENCES [dbo].[Transaction]([Id]), 
 )
---
+++ MarvelousService.DB/Tables/Transaction.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Transaction]
+(
+	[Id] INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+	[ServiceToLeadId] INT NOT NULL,
+    CONSTRAINT [FK_ServiceToLead_Transaction_ServiceToLead] FOREIGN KEY ([ServiceToLeadId]) REFERENCES [dbo].[ServiceToLead]([Id])
+)
