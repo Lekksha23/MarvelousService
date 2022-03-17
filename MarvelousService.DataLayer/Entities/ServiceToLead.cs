db19fe0741a83894c2e81c0e6a9fbe33bb80d0cf
Lekksha <bennykorovin@gmail.com>
2022-03-17 18:21:43 +0300

    entity fixed

--- MarvelousService.DataLayer/Entities/ServiceToLead.cs
+++ MarvelousService.DataLayer/Entities/ServiceToLead.cs
@@ -5,7 +5,6 @@ namespace MarvelousService.DataLayer.Entities
     public class ServiceToLead
     {
         public int Id { get; set; }
-        public ServiceType Type { get; set; }
         public Period Period { get; set; }
         public decimal Price { get; set; }
         public Status Status{ get; set; }
