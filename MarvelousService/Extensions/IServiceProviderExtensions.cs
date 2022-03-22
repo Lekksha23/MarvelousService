870b558bf13fc85f72308ba5e3589cb18f0613ec
Aleksandr <shohin-sasha1993@yandex.ru>
2022-03-22 23:21:11 +0300

    Add

--- MarvelousService/Extensions/IServiceProviderExtensions.cs
+++ MarvelousService/Extensions/IServiceProviderExtensions.cs
@@ -1,10 +1,14 @@
 ﻿using MarvelousService.API.Configuration;
+using MarvelousService.BusinessLayer.Configuration;
 using MarvelousService.BusinessLayer.Configurations;
 using MarvelousService.BusinessLayer.Services;
 using MarvelousService.BusinessLayer.Services.Interfaces;
 using MarvelousService.DataLayer.Interfaces;
 using MarvelousService.DataLayer.Repositories;
 using MarvelousService.DataLayer.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using NLog.Extensions.Logging;
 
 namespace MarvelousService.API.Extensions
@@ -38,6 +42,66 @@ namespace MarvelousService.API.Extensions
                 loggingBuilder.SetMinimumLevel(LogLevel.Information);
                 loggingBuilder.AddNLog(config);
             });
+        }
+
+        public static void RegisterSwaggerAuth(this IServiceCollection swagger)
+        {
+            swagger.AddSwaggerGen(opt =>
+            {
+                opt.EnableAnnotations();
+                opt.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "MyAPI",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Git Repository",
+                        Url = new Uri("https://github.com/Lekksha23/MarvelousServic"),
+                    }
+                });
+                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer"
+                });
+
+                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                        Reference = new OpenApiReference
+                        {
+                            Type=ReferenceType.SecurityScheme,
+                            Id="Bearer"
+                        }
+                    },
+                    new string[]{}
+                }
+                });
+            });
+        }
+
+        public static void RegisterAuthJwtToken(this IServiceCollection jwt)
+        {
+            jwt.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidIssuer = AuthOptions.Issuer,
+                        ValidateAudience = true,
+                        ValidAudience = AuthOptions.Audience,
+                        ValidateLifetime = true,
+                        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+                        ValidateIssuerSigningKey = true,
+                    };
+                });
         }
     }
 }
