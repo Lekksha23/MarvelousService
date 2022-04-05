59bffc540fa739ed03e6299b9a56ab0b1ec604a7
Lekksha <bennykorovin@gmail.com>
2022-04-05 13:03:59 +0300

    added new storedprocedure

---
+++ MarvelousService.DB/StoredProcedures/LeadResource/LeadResource_SelectByPayDate.sql
@@ -0,0 +1,22 @@
+﻿create procedure [dbo].[LeadResource_SelectByPayDate]
+   @Date date
+as
+begin
+	create table #ResourceInfo
+	(Id int, 
+	 LeadId int,
+	 ResourceId int,
+	 Price decimal (6,0))
+insert into #ResourceInfo (Id, LeadId, ResourceId, Price) 
+select 
+	Id, 
+	LeadId, 
+	ResourceId, 
+	Price 
+from dbo.LeadResource
+where datediff(week, StartDate, @Date) = 1 and [Status] = 1 and [Period] = 2 or 
+	  datediff(month, StartDate, @Date) = 1 and [Status] = 1 and [Period] = 3 or 
+	  datediff(year, StartDate, @Date) = 1 and [Status] = 1 and [Period] = 4
+end
+
+select * from #ResourceInfo
--- MarvelousService.DB/Tables/LeadResource.sql
+++ MarvelousService.DB/Tables/LeadResource.sql
@@ -4,7 +4,8 @@
 	[Price] decimal(10,0) NOT NULL,
 	[Status] int NOT NULL,
 	[Period] int NOT NULL,
+	[StartDate] date,
 	[ResourceId] int NOT NULL,
-	[LeadId] int NOT NULL,  
+	[LeadId] int NOT NULL
 	CONSTRAINT [FK_LeadResource_Resource] FOREIGN KEY ([ResourceId]) REFERENCES [dbo].[Resource]([Id])
 )
