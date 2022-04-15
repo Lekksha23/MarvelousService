afb4bd4b542076aa148c5f34e8db1b81daa30443
Aleksandr <shohin-sasha1993@yandex.ru>
2022-04-15 20:36:59 +0300

    Fix

--- MarvelousService.API.Tests/ResourceControllerTests.cs
+++ MarvelousService.API.Tests/ResourceControllerTests.cs
@@ -7,11 +7,13 @@ using MarvelousService.API.Validators;
 using MarvelousService.BusinessLayer.Clients.Interfaces;
 using MarvelousService.BusinessLayer.Configurations;
 using MarvelousService.BusinessLayer.Helpers;
+using MarvelousService.BusinessLayer.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Moq;
 using NUnit.Framework;
 using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace MarvelousService.API.Tests
 {
@@ -23,6 +25,8 @@ namespace MarvelousService.API.Tests
         private readonly IMapper _autoMapper;
         private readonly Mock<IRequestHelper> _requestHelper;
         private readonly IValidator<ResourceInsertRequest> _validatorResourceInsertRequest;
+        private readonly IValidator<ResourceSoftDeleteRequest> _validatorResourceSoftDeletetRequest;
+        private readonly IValidator<ResourceUpdateRequest> _validatorResourceUpdatetRequest;
         private readonly Mock<IResourceProducer> _resourceProducer;
 
 
@@ -33,21 +37,72 @@ namespace MarvelousService.API.Tests
             _configuration = new Mock<IConfiguration>();
             _autoMapper = new Mapper(new MapperConfiguration(cfg => cfg.AddProfile<AutoMapperToData>()));
             _validatorResourceInsertRequest = new ResourceInsertRequestValidator();
+            _validatorResourceSoftDeletetRequest = new ResourceSoftDeleteRequestValidator();
+            _validatorResourceUpdatetRequest = new ResourceUpdateRequestValidator();
             _resourceProducer = new Mock<IResourceProducer>();
             _requestHelper = new Mock<IRequestHelper>();
         }
 
+        [Test]
+        public async Task ResourceTestIssueAnDuplicationException()
+        {
+            // Arrange
+            var resourceId = 1;
+            var resourceNew = new ResourceModel
+            {
+                Id = 1,
+                Name = "ewq",
+                Description = "EWQEWQ",
+                Price = 3000,
+                IsDeleted = false,
+            };
+            var resoerceRequestModel = new ResourceInsertRequest
+            {
+                
+                Name = "qwe",
+                Description = "EWQEWQ",
+                Price = 1500,
+                Type = 0
+            };
+            
+            
+            _resourceService.Setup(t => t.GetResourceById(resourceId)).ReturnsAsync(resourceNew);
+            var resourseModel = _autoMapper.Map<ResourceModel>(resoerceRequestModel);
+
+            //when
+            var actual =  _controller.AddResource(resoerceRequestModel);
+
+            //then
+            _resourceService.Verify(m => m.AddResource(It.IsAny<ResourceModel>()), Times.Once());
+
+        }
+
+
         //[Test]
-        //public async Task ResourceTestIssueAnDuplicationException()
+        //public void SoftDeleteReturnsOk()
         //{
+        //    // Arrange
+        //    var resourceId = 1;
+        //    var resource = new ResourceResponse
+        //    {
+        //        Id = 1,
+        //        Name = "ewq",
+        //        Description = "EWQEWQ",
+        //        Price = 3000,   
+                
+        //    };
+        //    string token = "token";
+        //    var context = new DefaultHttpContext();
+        //    context.Request.Headers.Authorization = token;
+        //    _controller.ControllerContext.HttpContext = context;
+        //    var resourseModel = _autoMapper.Map<ResourceModel>(resource);
+            
+        //    // Act
+        //     _controller.(10);
 
-           
-
-
+        //    // Assert
+        //    Assert.IsInstanceOfType(actionResult, typeof(OkResult));
         //}
-
-
-
 
 
 
