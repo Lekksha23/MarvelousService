f16beebf737078a19f7077e00a2dfe9e02d11047
Aleksandr <shohin-sasha1993@yandex.ru>
2022-04-15 03:22:28 +0300

    Add Api test and Validator

---
+++ MarvelousService/Validators/ResourceInsertRequestValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using MarvelousService.API.Models;
+using System.Text.RegularExpressions;
+
+namespace MarvelousService.API.Validators
+{
+    public class ResourceInsertRequestValidator : AbstractValidator<ResourceInsertRequest>
+    {
+        public ResourceInsertRequestValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty()
+                .Matches(new Regex(@"^[а-яА-ЯёЁa-zA-Z]+$"))
+                .WithMessage("Unavailable name");
+            RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x.Price)
+                //.Match(new Regex(@"[0-9]+.[0-9]{2}$"))
+                .NotEmpty()
+                .WithMessage("Birthday is empty");
+            RuleFor(x => x.Type)
+                //.Matches(new Regex(@"^[\+?(?:[0-9]?){6,14}[0-9]]+$"))
+                .NotEmpty()
+                .IsInEnum();
+
+        }
+    }
+}
