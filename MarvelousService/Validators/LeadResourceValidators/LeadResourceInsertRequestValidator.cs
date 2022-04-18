e322d74fcc6a970f26f5a29406d424dbbb40d67f
Lekksha <bennykorovin@gmail.com>
2022-04-18 11:51:46 +0300

    Validation started

---
+++ MarvelousService/Validators/LeadResourceValidators/LeadResourceInsertRequestValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using MarvelousService.API.Models;
+
+namespace MarvelousService.API.Validators
+{
+    public class LeadResourceInsertRequestValidator : AbstractValidator<LeadResourceInsertRequest>
+    {
+        public LeadResourceInsertRequestValidator()
+        {
+            RuleFor(x => x.ResourceId)
+                .NotEmpty();
+                
+        }
+    }
+}
